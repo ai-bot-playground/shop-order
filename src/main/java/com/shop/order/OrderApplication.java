8b0782dc24cc7b1edf10d3273398e6f96f4ab19b
Mandervain <kmnski.l@gmail.com>
2026-06-29 17:50:47 +0200

    feat: add startup logging in main application classes

--- src/main/java/com/shop/order/OrderApplication.java
+++ src/main/java/com/shop/order/OrderApplication.java
@@ -1,12 +1,18 @@
 package com.shop.order;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderApplication.class);
+
     public static void main(String[] args) {
+        log.info("Starting OrderApplication...");
         SpringApplication.run(OrderApplication.class, args);
     }
+
 }
